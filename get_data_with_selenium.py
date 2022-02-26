be954013a3cd0b8610e49a5d3dcad41709f63755
Vitykos <vik.kostyrin@gmail.com>
2022-02-26 14:45:29 +0300

    add dalay funk

--- get_data_with_selenium.py
+++ get_data_with_selenium.py
@@ -1,16 +1,24 @@
-from selenium import webdriver
+import time
 import os
 
+delay = 1
 
 def download_libraries():
     os.system("pip3 install selenium")
 
 
+def set_delay(t):
+    delay = t
+
+
 def get_html_code_by(url):
+    from selenium import webdriver
+
     driver = webdriver.Chrome(executable_path="/Users/viktor/PycharmProjects/selenium_python/chromedriver")
 
     try:
         driver.get(url)
+        time.sleep(delay)
         return driver.page_source
 
     except Exception as ex:
